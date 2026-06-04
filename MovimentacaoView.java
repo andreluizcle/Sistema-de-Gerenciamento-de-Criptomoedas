b056cc49a870cdb46ce727b13c5e661843ebe340
BeatrizAmaral-251006 <beatriz.amaral.2006@gmail.com>
2026-06-04 11:37:08 -0300

    Refactor MovimentacaoView for improved structure

--- MovimentacaoView.java
+++ MovimentacaoView.java
@@ -1,68 +1,64 @@
-package java.view;
+package br.unicamp.ftcoin.view;
 
-import java.MessageProvider;
-import java.dto.Movimentacao;
-import java.util.List;
-import java.util.Scanner;
+import br.unicamp.ftcoin.dto.Movimentacao;
+import br.unicamp.ftcoin.dto.Oraculo;
+import br.unicamp.ftcoin.util.Console;
+import br.unicamp.ftcoin.util.MessageProvider;
 
-public class MovimentacaoView {
-    private Scanner scanner;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
-    public MovimentacaoView() {
-        this.scanner = new Scanner(System.in);
-    }
+/**
+ * View responsavel pela tela de Movimentacao (compra / venda).
+ */
+public class MovimentacaoView extends ViewBase {
 
     public int exibirMenu() {
-        System.out.println("\n" + MessageProvider.get("transaction.title"));
-        System.out.println(MessageProvider.get("transaction.buy"));
-        System.out.println(MessageProvider.get("transaction.sell"));
-        System.out.println(MessageProvider.get("transaction.list"));
-        System.out.println("0. Voltar");
-        System.out.print(MessageProvider.get("menu.option") + " ");
-        
-        try {
-            return Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            return -1;
+        cabecalho("menu.movimentacao.titulo");
+        System.out.println(MessageProvider.get("menu.movimentacao.1"));
+        System.out.println(MessageProvider.get("menu.movimentacao.2"));
+        System.out.println(MessageProvider.get("menu.movimentacao.0"));
+        return lerInteiro("menu.principal.escolha");
+    }
+
+    public int lerIdCarteira() {
+        return lerInteiro("mov.entrada.id.carteira");
+    }
+
+    public BigDecimal lerQuantidade() {
+        return lerDecimal("mov.entrada.quantidade");
+    }
+
+    public LocalDate lerData() {
+        return lerData("mov.entrada.data");
+    }
+
+    public void mensagemCompraOk(Movimentacao mov) {
+        Console.sucesso(MessageProvider.get("mov.msg.compra.ok", mov.getIdMovimento()));
+    }
+
+    public void mensagemVendaOk(Movimentacao mov) {
+        Console.sucesso(MessageProvider.get("mov.msg.venda.ok", mov.getIdMovimento()));
+    }
+
+    public void mensagemSaldoInsuficiente(BigDecimal saldo) {
+        Console.erro(MessageProvider.get("mov.msg.saldo.insuficiente", saldo.toPlainString()));
+    }
+
+    public void mensagemQuantidadeInvalida() {
+        Console.erro(MessageProvider.get("mov.msg.quantidade.invalida"));
+    }
+
+    public void exibirCotacaoUsada(Oraculo cot) {
+        if (cot == null) {
+            Console.aviso(MessageProvider.get("oraculo.cotacao.indisponivel"));
+            return;
         }
+        Console.info(MessageProvider.get("mov.msg.cotacao.usada",
+                cot.getCotacao().toPlainString() + " (" + cot.getData() + ")"));
     }
 
-    public int solicitarIdCarteira() {
-        System.out.print(MessageProvider.get("transaction.wallet_id") + " ");
-        return Integer.parseInt(scanner.nextLine());
-    }
-
-    public String solicitarCriptomoeda() {
-        System.out.print(MessageProvider.get("transaction.crypto") + " ");
-        return scanner.nextLine().toUpperCase();
-    }
-
-    public double solicitarQuantidade() {
-        System.out.print(MessageProvider.get("transaction.amount") + " ");
-        return Double.parseDouble(scanner.nextLine());
-    }
-
-    public double solicitarCotacao() {
-        System.out.print(MessageProvider.get("transaction.price") + " ");
-        return Double.parseDouble(scanner.nextLine());
-    }
-
-    public void listarMovimentacoes(List<Movimentacao> movimentacoes) {
-        System.out.println("\n--- Extrato de Movimentações ---");
-        if (movimentacoes.isEmpty()) {
-            System.out.println("Nenhuma movimentação encontrada para esta carteira.");
-        } else {
-            for (Movimentacao m : movimentacoes) {
-                System.out.println(m.toString());
-            }
-        }
-    }
-
-    public void exibirSucesso() {
-        System.out.println(MessageProvider.get("msg.success"));
-    }
-
-    public void exibirMensagemInvalida() {
-        System.out.println(MessageProvider.get("msg.invalid"));
+    public void mensagemErro(String chave) {
+        Console.erro(MessageProvider.get(chave));
     }
 }
